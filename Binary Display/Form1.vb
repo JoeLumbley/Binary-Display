c0d698899476db2c7d2faab8ae986290190ac41e
Joseph Lumbley <joelumbley16@gmail.com>
2025-11-18 11:35:06 -0600

    Worked on font scaleing

--- Binary Display/Form1.vb
+++ Binary Display/Form1.vb
@@ -244,6 +244,13 @@ Public Class Form1
                 PlaceValueFont = New Font("Consolas", Math.Max(6, Me.ClientSize.Height \ 28))
                 BreakdownFont = New Font("Consolas", Math.Max(6, Me.ClientSize.Height \ 30))
                 DecimalFont = New Font("Consolas", Math.Max(12, Me.ClientSize.Height \ 12))
+
+            Case = 1.75F
+                BitBoxFont = New Font("Consolas", Math.Max(20, Me.ClientSize.Height \ 22))
+                PlaceValueFont = New Font("Consolas", Math.Max(6, Me.ClientSize.Height \ 32))
+                BreakdownFont = New Font("Consolas", Math.Max(6, Me.ClientSize.Height \ 35))
+                DecimalFont = New Font("Consolas", Math.Max(12, Me.ClientSize.Height \ 14))
+
         End Select
 
     End Sub
