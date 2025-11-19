430c4a3a6ce9d4b40de4d719b9f57cf2bbc1e076
Joseph Lumbley <joelumbley16@gmail.com>
2025-11-19 12:28:06 -0600

    Reduce "ComputerPulsation" sound volume to 5
    The volume level for the "ComputerPulsation" sound was adjusted
    from 40 to 5 using the `Player.SetVolume` method to lower the
    audio output.

--- Binary Display/Form1.vb
+++ Binary Display/Form1.vb
@@ -189,7 +189,7 @@ Public Class Form1
 
         Player.AddSound("ComputerPulsation", FilePath)
 
-        Player.SetVolume("ComputerPulsation", 40)
+        Player.SetVolume("ComputerPulsation", 5)
 
         MinimumSize = New Size(720, 480)
 
